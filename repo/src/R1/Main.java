9727c8d11ea2d70f98b19c75e1b8406feeca39ae
admin <admin@DESKTOP-TJSP1RG>
2023-05-26 17:56:29 +0530

    version 1.0

--- repo/src/R1/Main.java
+++ repo/src/R1/Main.java
@@ -3,5 +3,6 @@ package R1;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("THIS IS THE MAIN VERSION");
+		System.out.println("THIS IS THE VERSION 1.0");
 	}
 }
